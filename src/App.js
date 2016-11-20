bcd35a1b7efa529394402d5601bb7280944bd8d6
Peter Doane <peterdoane@gmail.com>
2016-11-19 20:23:33 -0800

    router & ejected

--- src/App.js
+++ src/App.js
@@ -5,27 +5,18 @@ import Header from './Header';
 import LogoHeader from './LogoHeader'
 import ImageGallery from 'react-image-gallery';
 import Section from './Section';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <LogoHeader />
-        <ImageGallery
-          slideInterval={2000}
-          autoPlay
-          items={[{
-        original: 'http://lorempixel.com/1000/600/nature/2/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/3/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
-      }]}
-        />
-        <Section backgroundUrl="http://360modern.com/wp-content/themes/360-redux/_assets/images/bg-topmiddle.jpg">HeyHeyHey</Section>
-      </div>
+      <Router history={browserHistory}>
+       <Route path="/" component={MainLayout}>
+         <IndexRoute component={HomePage} />
+       </Route>
+     </Router>
     );
   }
 }
