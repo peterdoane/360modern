c7f750c0c6dcadf42b89ab3bf58baf8e232f5d3c
Peter Doane <peterdoane@gmail.com>
2016-11-19 20:36:53 -0800

    Added SubHeader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import LogoHeader from './LogoHeader'
-import ImageGallery from 'react-image-gallery';
-import Section from './Section';
+
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './MainLayout.css';
 import Header from '../../components/Header';
+import SubHeader from '../../components/SubHeader';
+
 const MainLayout = ({ children }) => (
   <div>
     <Header />
+    <SubHeader />
     <div>{children}</div>
     <div className={styles.footer}></div>
   </div>
